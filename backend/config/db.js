a2fd5a6d68f64c1dc8edd313f84f8b13aeb3e810
jenil <jenilsakriya612@gmail.com>
2025-06-02 13:33:46 +0530

    db connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://jenilsakariya:wuldu2FQnslbPjM8@cluster0.xeuswyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(MONGO_URI);
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
